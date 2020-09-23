e1db3af84c21279183c19f95625d86018f8af173
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-09-23 11:58:08 -0700

    created a start button and added functionality to it

--- app/app.js
+++ app/app.js
@@ -5,9 +5,10 @@ let play = $('#play')
 let hungervalue = $('#hunger').val()
 let energyvalue = $('#tiredness').val()
 let boredvalue = $('#boredom').val()
+let start = $('#start')
 
 class Tomogachi {
-    constructor(hungervalue){
+    constructor(hungervalue, energyvalue, boredvalue){
     this.hunger = hungervalue
     this.energy = energyvalue
     this.bored = boredvalue 
@@ -39,8 +40,40 @@ play = function() {
 
 let mario = new Tomogachi
 
-//**************************** BUTTON FUNCTIONS */
+//**************************** BUTTON EVENT LISTENERS *****************************/
 feed.on('click', mario.feed)
 sleep.on('click', mario.sleep)
 play.on('click', mario.play)
+start.on('click', startTimer)
+
+
+
+
+//**************************** TIMER FUNCTIONS ******************************** */
+
+function timerEffects() {
+    if(hungervalue <= 0 || energyvalue <= 0 || boredvalue <= 0) {
+        $('#screen').html('<h1> YOU LET MARIO DIE <br><br>GAME OVER</h1>')
+        
+    }else{
+    hungervalue -= .3
+    energyvalue -= .2
+    boredvalue -= .1
+    $('#hunger').val(hungervalue)
+    $('#tiredness').val(energyvalue)
+    $('#boredom').val(boredvalue)
+}
+}
+
+function startTimer() {
+ setInterval(timerEffects, 1000)
+}
+
+
+
+
+
+
+
+
 
